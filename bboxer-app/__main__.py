194383bdf4616253e85b0f56ab6d09f677abb229
Kacper Kedzierski <kacper.kedzierski1986@gmail.com>
2022-10-20 20:58:49 +0200

    Add boilerplate

---
+++ bboxer-app/__main__.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main(args: argparse.Namespace) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Detect objects")
+
+    args = parser.parse_args()
+
+    main()
